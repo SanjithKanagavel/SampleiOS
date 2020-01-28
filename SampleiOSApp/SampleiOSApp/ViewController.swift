e649efe87aee60a311facfc8f1838ee56fd30f75
Sanjith Jothi Kanagavel <sanjith@freshdesk.com>
2020-01-28 16:27:31 +0530

    Code changes.

---
+++ SampleiOSApp/SampleiOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleiOSApp
+//
+//  Created by Sanjith Kanagavel on 28/01/20.
+//  Copyright © 2020 Sanjith Kanagavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
